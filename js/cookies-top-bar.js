8b944b61c0ba41a8c518dc65db4a5ce6d9313085
robfree <robertoserrano83@gmail.com>
2015-04-28 01:41:45 +0200

    remove console.log

--- js/cookies-top-bar.js
+++ js/cookies-top-bar.js
@@ -1,7 +1,6 @@
 $(function() {
 	var cname = cname || undefined;
 	function cookiesTopBar(cname) {
-		// obtengo valor de la cookie pasada a la funcion
 		cname = decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" +
 		 encodeURIComponent(cname).replace(/[\-\.\+\*]/g, "\\$&") +
 		  "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
@@ -20,17 +19,14 @@ $(function() {
  	}
 	$("#cookie-click").on("click", function() {
 		document.cookie = "acceptcookies=yes";
-		console.log(document.cookie);
 
 		function swing() {
-			console.log('Efecto entransicion');
 			$( ".center" ).fadeOut( "fast" );
 		}
 
 		$('#cookies-top-bar').slideUp(400, swing(), function() {
-				console.log("Efecto en completado");
+
 		});
-		// $("#cookies-top-bar").css("display", "none");
 	});
 	cookiesTopBar("acceptcookies");
 });
